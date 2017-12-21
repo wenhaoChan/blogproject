de34d5262b9f7f3e2d20d11ccb3dd1befa3fb08c
wenhaoChan <695373135@qq.com>
2017-12-21 15:27:20 +0800

    修改配置

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '510ms1)m%#6&_sct^e3s%uf-iy2nybl+%cf6mu6box^qgyou5j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zerochan.xin', 'http://120.79.69.170/']
 
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog_db',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
