3afd6fb9b7ffc3c2c3c53a221617cee7c41fe9ed
wenhaoChan <695373135@qq.com>
2018-01-05 14:31:33 +0800

    让pip中的mysql-python支持python3.x

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,11 +1,7 @@
 from os import path
 from django.apps import AppConfig
-import pymysql
-
-pymysql.install_as_MySQLdb()  # 让pip中的mysql-python支持python3.x
 
 VERBOSE_APP_NAME = u"文章"
-
 
 def get_current_app_name(file):
     return path.dirname(file).replace('\\', '/').split('/')[-1]
@@ -16,4 +12,4 @@ class AppVerboseNameConfig(AppConfig):
     verbose_name = VERBOSE_APP_NAME
 
 
-default_app_config = AppVerboseNameConfig
+default_app_config = get_current_app_name(__file__) + '.__init__.AppVerboseNameConfig'
---
+++ blogproject/__init__.py
@@ -0,0 +1,3 @@
+import pymysql
+
+pymysql.install_as_MySQLdb()  # 让pip中的mysql-python支持python3.x
